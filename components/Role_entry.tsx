a2acceaff2b7daa2ce4627851aa2e7e822d773b5
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-09-21 15:28:33 +0200

    Started with the roles

---
+++ components/Role_entry.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  role: string
+  role2: string
+  previous: string
+  text: string
+}
+
+const Role_entry = (props: Props) => {
+  return (
+    <div>
+      <hr className='my-6 w-5/6 m-auto h-[2px] border-0 rounded bg-whiteText' />
+      <div className='px-8'>
+        <h3 className="new-h3">{props.role}</h3>
+        {props.role2 != "" ? <h4 className='new-h4 pl-3'>({props.role2})</h4> : null}
+        <p className='pl-10 mt-4'>
+          {props.text}
+        </p>
+        <p className="font-modestoExpanded text-2xl pl-10 mt-2">
+          Skurkeriet 23/24:s {props.role.split(',')[0] + ": " + props.previous}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Role_entry
---
+++ data/role_data.tsx
@@ -0,0 +1,34 @@
+export const roles = [
+  {
+    id: "annie",
+    name: "Annie Brattlund",
+    role: "General",
+    role2: "Storskurk",
+    text:
+      "Som General är det ditt ansvar att se till att allt arbete flyter på som det ska i gruppen och du fungerar som en projektledare för Skurkeriets arbete. Som General har du huvudansvar för allt som Skurkeriet gör och du är Skurkeriets röst utåt. Du kommer som General sitta i en generalstvägrupp där du och övriga Generaler och Vice Generaler samarbetar och samordnar allt arbete inför Nolle-p tillsammans med Mottagningsansvariga, en representant från LinTek. Utöver Skurkeriet och generalstvärgruppen kommer du hålla i en egen tvärgrupp. En tvärgrupp är en grupp där alla med samma post från de olika phadderierna för Linteks mottagning i Norrköping jobbar gemensamt.",
+  },
+  {
+    id: "emil",
+    name: "Emil Alsbjer",
+    role: "Vice General, Logistikansvarig och Sekreterare",
+    role2: "Tjallare",
+    text:
+      "Som Logistikansvarig i Skurkeriet är det ditt ansvar att till exempel boka salar, förbereda protokoll och ordna med schemaläggning. Din roll som Sekreterare för Skurkeriet är en del av Vice posten, så det är viktigt att du kan skriva tydliga och välstrukturerade protokoll för att övriga i phadderiet ska veta vad som har bestämts. Som Vice General för skurkeriet är du Generalens högra hand. Kan Generalen inte medverka är det du som tar över. Det är viktigt att ha bra kontakt med Generalen och strukturera upp arbetet så det underlättar för både dig och Generalen. Precis som Generalen kommer du som Vice General att ansvara för en tvärgrupp när planeringen inför nolle-p drar igång samt vara med i Generals-tvärgruppen.",
+  },
+  {
+    id: "oscar",
+    name: "Oscar Fyrk",
+    role: "Kassör",
+    role2: "Falskmyntare",
+    text:
+      "Som Kassör har du har ansvar över det ekonomiska. Alla ekonomiska beslut går via dig. Du ska även ha koll på Skurkeriets budget och se till att alla håller sig till de ekonomiska beslut som tagits.",
+  },
+  {
+    id: "alice",
+    name: "Alice Pälvärinne",
+    role: "Tryck och PR ansvarig",
+    role2: "",
+    text:
+      "Som Tryck- och PR har du det främsta ansvaret för Skurkeriets visuella uttryck. Det kan handla om allt från att göra märken och marknadsföring till att skapa phadderiets idolkort. Varje år tas även en handbok fram till Basårs-Nollan, vilket också detta är ditt ansvar som Tryck- och PR. Det finns inget krav på tidigare erfarenhet, men kunskaper inom grafisk design och Adobe-programmen är meriterande.",
+  },
+];
