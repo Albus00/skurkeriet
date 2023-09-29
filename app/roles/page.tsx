22c00df013e458c11ed62b77c5a9e568a14d16c6
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-09-29 10:50:31 +0200

    Done :)

---
+++ app/roles/page.tsx
@@ -0,0 +1,10 @@
+import Roles from '@/app/Roles';
+
+export default function Page() {
+  return (
+    <main className='pt-14'>
+      {/* Render the roles */}
+      <Roles />
+    </main >
+  )
+}
