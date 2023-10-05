6d5345c6a2609f86c854348ff84af243940b06b6
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-10-05 17:22:28 +0200

    Fixed issues with the repo :)

--- components/Role_entry.tsx
+++ components/Role_entry.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 type Props = {
   role: string
   role2: string
@@ -6,20 +8,35 @@ type Props = {
 }
 
 const Role_entry = (props: Props) => {
+  // Find webpages embedded in the role text and split the string
+  let webText = props.text.split('&a');
+
+
   return (
     <div>
       <hr className='my-6 w-5/6 m-auto h-[2px] border-0 rounded bg-whiteText' />
       <div className='px-8'>
         <h3 className="new-h3">{props.role}</h3>
         {props.role2 != "" ? <h4 className='new-h4 pl-3'>({props.role2})</h4> : null}
-        <p className='pl-10 mt-4 mobile:pl-0'>
-          {props.text}
-        </p>
+        <div className='pl-10 mt-4 mobile:pl-0'>
+          {/* If webpages are found; add them in between the splits in the text */}
+          {webText.length > 1 ?
+            (
+              <p>
+                {webText[0]}
+                <a className="font-semibold hover:underline" href="/?redir=true">skurkeriets</a>
+                {webText[1]}
+                <a className="font-semibold hover:underline" href="https://basaret.se">basårets</a>
+                {webText[2]}
+              </p>
+            ) : (
+              <p>{props.text}</p>)}
+        </div>
         <p className="font-modestoExpanded text-2xl pl-10 mt-2 mobile:text-lg mobile:pl-0">
           Skurkeriet 23/24:s {props.role.split(',')[0] + ": " + props.previous}
         </p>
       </div>
-    </div>
+    </div >
   )
 }
 
