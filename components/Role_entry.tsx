23579c1356cb9d2c731b91c0fc6f544a9426ed27
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-10-05 20:35:44 +0200

    small fixes

--- components/Role_entry.tsx
+++ components/Role_entry.tsx
@@ -14,8 +14,8 @@ const Role_entry = (props: Props) => {
 
   return (
     <div>
-      <hr className='my-6 w-5/6 m-auto h-[2px] border-0 rounded bg-whiteText' />
-      <div className='px-8'>
+      <hr className='my-6 w-[90%] m-auto h-[2px] border-0 rounded bg-whiteText' />
+      <div className='px-6'>
         <h3 className="new-h3">{props.role}</h3>
         {props.role2 != "" ? <h4 className='new-h4 pl-3'>({props.role2})</h4> : null}
         <div className='pl-10 mt-4 mobile:pl-0'>
@@ -34,7 +34,7 @@ const Role_entry = (props: Props) => {
         </div>
         <div className="font-modestoExpanded text-2xl pl-10 mt-2 mobile:text-lg mobile:pl-0">
           <p className="text-left inline handheld:text-xl mobile:text-base">Skurkeriet 23/24:s {props.role.split(',')[0] + ": "}</p>
-          <p className="inline handheld:block">{props.previous}</p>
+          <p className="inline handheld:block mobile:underline">{props.previous}</p>
         </div>
       </div>
     </div >
